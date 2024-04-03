f8602aef4e5dd378fac7aa5ab2187ff0a0df2287
layzaaa <128812746+layzaaa@users.noreply.github.com>
2024-04-03 12:43:43 -0300

    03/04

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes = document.querySelectorAll (".botao");
+const textos = document. querySelectorAll("aba-conteudo");
+for(let i=0; i<botoes.length; i++){
+    botoes [i].oneclick = function(){
+        for(let j=0; j<botoes.length; j++){
+            
+        }
+    }
+}
